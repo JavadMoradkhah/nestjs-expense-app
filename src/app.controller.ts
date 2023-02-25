9c7becdd7404fad1eced62291d2ac6e8dc0e81b0
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-02-25 21:56:59 +0330

    feat: Adding more methods to the app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
-@Controller()
+@Controller('/report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
+  getAllReports() {
     return [];
+  }
+
+  @Get(':id')
+  getReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
