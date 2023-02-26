f87c6fb873ab8685e7d9bee0a889c579649a3400
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-02-26 18:28:34 +0330

    feat: Implementing POST, PUT & Delete routes in the app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,84 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { data, ReportType, Report } from './data';
 
 @Controller('/report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    return data.report.filter((report) => report.type === type);
   }
 
   @Get(':id')
-  getReportById() {
-    return {};
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType: ReportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
   }
 
   @Post()
-  createReport() {
-    return 'Created';
+  createReport(
+    @Param('type') type: string,
+    @Body() body: { source: string; amount: number },
+  ) {
+    const newReport: Report = {
+      id: uuid(),
+      source: body.source,
+      amount: body.amount,
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    data.report.push(newReport);
+
+    return newReport;
   }
 
   @Put(':id')
-  updateReport() {
-    return 'Updated';
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { source: string; amount: number },
+  ) {
+    const reportType: ReportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    const report = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+
+    if (!report) return;
+
+    Object.assign(report, body);
+
+    return report;
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport() {
-    return 'Deleted';
+  deleteReport(@Param('type') type: string, @Param('id') id: string) {
+    const reportType: ReportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    const reportIndex = data.report
+      .filter((report) => report.type === reportType)
+      .findIndex((report) => report.id === id);
+
+    data.report.splice(reportIndex, 1);
+
+    return;
   }
 }
